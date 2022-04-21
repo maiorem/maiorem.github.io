48fb8b777490ed4092b127d63adba900ac319a07
Seyoung Hong <maiorem00@gmail.com>
2022-04-21 10:49:39 +0900

    intro

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `Ἥφαιστος`,
+  description: `코드 짓는 타자기`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https:/maiorem.github.io`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
@@ -11,16 +11,16 @@ module.exports = {
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `홍세영`,
     bio: {
       role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
+      description: ['글쓰는', '공부하는', '사람에 가치를 두는', '즐거운'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/zoomKoding`,
-      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `zoomkoding@gmail.com`,
+      github: `https://github.com/maiorem`, // `https://github.com/maiorem`,
+      linkedIn: `https://www.linkedin.com/in/seyoung-hong-8155b21bb`, // `https://www.linkedin.com/in/seyoung-hong-8155b21bb`,
+      email: `maiorem00@gmail.com`, // `maiorem00@gmail.com`,
     },
   },
 
@@ -43,7 +43,7 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        date: '2021.02 ~',
+        date: '2022.04 ~',
         activity: '개인 블로그 개발 및 운영',
         links: {
           post: '/gatsby-starter-zoomkoding-introduction',
