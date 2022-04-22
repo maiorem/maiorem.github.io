b9a7b858d2cc1d7cf8d05a7b8a1e377015b2a682
Seyoung Hong <maiorem00@gmail.com>
2022-04-22 18:02:10 +0900

    gatsby update

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomePage({ data }) {
 
   return (
     <Layout>
-      <Seo title="Home" />
+      <Seo title="Ἥφαιστος" />
       <Bio author={author} language={language} />
       <PostTabs
         posts={posts}
