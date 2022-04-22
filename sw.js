e521880f275dc8554df2f9c7348a5931326f2a78
Seyoung Hong <maiorem00@gmail.com>
2022-04-22 13:54:26 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f0676e64c814686d584c.js"
+    "url": "webpack-runtime-9c0b554acfa07076ba1d.js"
   },
   {
     "url": "styles.853e65090dc89bfbb4ac.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "00179fd54ab2de3f6c786e1be6438dad"
+    "revision": "f5c5ad9d9918aefeee0daf544db1f4be"
   },
   {
     "url": "static/webfonts/s/roboto/v29/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
